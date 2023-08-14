c5c0eb67ed85f7bd4158f68cfbb2e8cded2bf180
Developer <developer@Developers-MacBook-Pro.local>
2023-08-14 18:39:37 +0800

    material theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
+
 import './App.css';
 
 function App() {
-	return <div className="a pp"></div>;
+	const mode = useSelector((state) => state.global.mode);
+	const theme = useMemo(
+		() => createTheme(themeSettings(mode)),
+		[mode]
+	);
+
+	return (
+		<div className="app">
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
