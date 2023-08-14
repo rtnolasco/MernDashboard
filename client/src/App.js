ecb64687846b26c60785f646a716c2eef92d8b1a
Developer <developer@Developers-MacBook-Pro.local>
2023-08-14 20:53:27 +0800

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,14 @@ import { useSelector } from 'react-redux';
 import { themeSettings } from 'theme';
 
 import './App.css';
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+import Dashboard from 'scenes/dashboard';
+import Layout from 'scenes/layout';
 
 function App() {
 	const mode = useSelector((state) => state.global.mode);
@@ -15,9 +23,25 @@ function App() {
 
 	return (
 		<div className="app">
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-			</ThemeProvider>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						<Route element={<Layout />}>
+							<Route
+								path="/"
+								element={
+									<Navigate to="/dashboard" replace />
+								}
+							/>
+							<Route
+								path="/dashboard"
+								element={<Dashboard />}
+							/>
+						</Route>
+					</Routes>
+				</ThemeProvider>
+			</BrowserRouter>
 		</div>
 	);
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import {
+	DarkModeOutlined,
+	LightModeOutlined,
+	Menu as MenuIcon,
+	Search,
+	SettingsOutlined,
+	ArrowDropDownOutlined,
+} from '@mui/icons-material';
+import FlexBetween from './FlexBetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import profileImage from 'assets/profile_pic.jpeg';
+import {
+	AppBar,
+	IconButton,
+	InputBase,
+	Toolbar,
+	useTheme,
+} from '@mui/material';
+
+const Navbar = () => {
+	const dispatch = useDispatch();
+	const theme = useTheme();
+	return (
+		<AppBar
+			sx={{
+				position: 'static',
+				backgrount: 'none',
+				boxShadow: 'none',
+			}}
+		>
+			<Toolbar sx={{ justifyContent: 'space-between' }}>
+				{/* {Left} */}
+				<FlexBetween>
+					<IconButton
+						onClick={() =>
+							console.log('open /close sideabar')
+						}
+					>
+						<MenuIcon />
+					</IconButton>
+					<FlexBetween
+						backgroundColor={theme.palette.background.alt}
+						borderRadius="9px"
+						gap="3rem"
+						p="0.1rem 1.5rem"
+					>
+						<InputBase placeholder="Search..." />
+						<IconButton>
+							<Search />
+						</IconButton>
+					</FlexBetween>
+				</FlexBetween>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+export default Navbar;
